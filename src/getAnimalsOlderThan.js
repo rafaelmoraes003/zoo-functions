282f7b32c924fbb6ab8b7b6e1a67e7d44b8ab6b1
rafael <rafaelsm003@gmail.com>
2022-03-16 20:48:11 -0300

    requisito 2 adicionado

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,17 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const { species } = data;
+
+  const myrray = [];
+
+  species.find((elemento) => elemento.name === animal).residents.forEach((idades) => {
+    myrray.push(idades.age);
+  });
+
+  return myrray.every((elementos) => elementos >= age);
 }
+
+//  console.log(getAnimalsOlderThan('otters', 7))
 
 module.exports = getAnimalsOlderThan;
