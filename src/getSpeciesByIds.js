3119df052099b55066b51427d3548c77ded3f293
rafael <rafaelsm003@gmail.com>
2022-03-16 19:18:06 -0300

    requisito 1 modificado

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -2,18 +2,17 @@ const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
-  const animalsId = [];
+  if (ids.length === 0) {
+    return [];
+  }
 
-  ids.forEach((parameters) => {
-    species.filter((elemento) => {
-      if (elemento.id === parameters) {
-        animalsId.push(elemento);
-      }
-      return elemento;
-    });
+  const myAnimal = [];
+  ids.forEach((id) => {
+    const animal = species.find((specie) => specie.id === id);
+    myAnimal.push(animal);
   });
 
-  return animalsId;
+  return myAnimal;
 }
 
 // console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce', '533bebf3-6bbe-41d8-9cdf-46f7d13b62ae'));
