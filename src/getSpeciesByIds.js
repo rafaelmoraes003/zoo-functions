295f24551c90f852234ed30435acdffffc77198e
rafael <rafaelsm003@gmail.com>
2022-03-16 17:55:36 -0300

    requisito 1 adicionado

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,21 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui - COMMIT INICIAL :)
+function getSpeciesByIds(...ids) {
+  const animalsId = [];
+
+  ids.forEach((parameters) => {
+    species.filter((elemento) => {
+      if (elemento.id === parameters) {
+        animalsId.push(elemento);
+      }
+      return elemento;
+    });
+  });
+
+  return animalsId;
 }
+
+// console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce', '533bebf3-6bbe-41d8-9cdf-46f7d13b62ae'));
 
 module.exports = getSpeciesByIds;
