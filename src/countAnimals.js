3ef00ae106dc80f3c51868f1afe0cb45455769cb
rafael <rafaelsm003@gmail.com>
2022-03-17 18:16:07 -0300

    requisito 5 adicionado

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,25 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    return species.reduce((acc, curreteValue) => {
+      acc[curreteValue.name] = curreteValue.residents.length; // UNDEFINED parameters
+      return acc;
+    }, {});
+  }
+
+  if (animal.sex) {
+    const animalName = species.find((elemento) => elemento.name === animal.specie);
+    const animalSex = animalName.residents.filter((elemento) => (elemento.sex === animal.sex)); // SPECIE and SEX
+    return animalSex.length;
+  }
+
+  const myAnimal = species.find((elemento) => elemento.name === animal.specie); // ONLY specie
+  return myAnimal.residents.length;
 }
+
+console.log(countAnimals({ specie: 'elephants', sex: 'male' }));
 
 module.exports = countAnimals;
