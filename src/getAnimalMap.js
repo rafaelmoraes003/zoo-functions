a8b35c73daa39d3d4d801e1c908bfdf31b299c09
rafael <rafaelsm003@gmail.com>
2022-03-21 18:53:45 -0300

    requisito 7 adicionado e projeto mais dificil ate agora FINALIZADO

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,53 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+const regions = ['NE', 'NW', 'SE', 'SW'];
+
+const noNameIncluded = () => {
+  const animalsByRegion = regions.reduce((acc, currentValue) => {
+    acc[currentValue] = species.filter((elemento) => elemento.location === currentValue)
+      .map((elemento1) => elemento1.name);
+    return acc;
+  }, {});
+  return animalsByRegion;
+};
+
+const names = (sorted, sex) => {
+  const myNames = regions.reduce((acc, currentValue) => {
+    acc[currentValue] = noNameIncluded()[currentValue].map((elemento) => {
+      const key = species.find((elemento1) => elemento1.name === elemento);
+      return { [key.name]: key.residents.map((elemento2) => elemento2.name) }; // SE SOMENTE INCLUDEDNAMES FOR PASSADO
+    });
+    return acc;
+  }, {});
+  return myNames;
+};
+
+const sortedOrSex = (sorted, sex) => {
+  const sla = regions.reduce((acc, currentValue) => {
+    acc[currentValue] = noNameIncluded()[currentValue].map((elemento) => {
+      const key = species.find((elemento1) => elemento1.name === elemento);
+      if (sorted && sex) {
+        return { [key.name]: key.residents.filter((elementoSx) => elementoSx.sex === sex)
+          .map((elementoSxName) => elementoSxName.name).sort(),
+        };
+      }
+      if (sorted) return { [key.name]: key.residents.map((elemento2) => elemento2.name).sort() };
+      return { [key.name]: key.residents.filter((elementoSx) => elementoSx.sex === sex)
+        .map((elementoSxName) => elementoSxName.name),
+      };
+    });
+    return acc;
+  }, {});
+  return sla;
+};
+
 function getAnimalMap(options) {
-  // seu código aqui
+  if (!options || !options.includeNames) return noNameIncluded();
+  if (options.sorted || options.sex) return sortedOrSex(options.sorted, options.sex);
+  return names();
 }
+
+console.log(getAnimalMap({ includeNames: true }));
 
 module.exports = getAnimalMap;
